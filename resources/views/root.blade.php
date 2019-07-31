2c7f020e18bbe0e4eddffb969b7e83c620204777
Efremenko Alex <alexeyefr@gmail.com>
2019-07-31 21:58:25 +0300

    Add: add the link to main page to main menu

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -13,6 +13,7 @@
         <script src="{{ asset('js/knockout-3.5.0.js') }}"></script>
         <script src="{{ asset('js/materialize.min.js') }}"></script>
         <script src="{{ asset('js/sammy-0.7.6.min.js') }}"></script>
+        <script src="{{ asset('js/moment.min.js') }}"></script>
         {{-- CSS style --}}
         <link href="{{ asset('css/materialize.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
@@ -24,12 +25,14 @@
                 <div class="nav-wrapper">
                     <a href="#" data-target="slide-out" class="sidenav-trigger show-on-med-and-down"><i class="material-icons">menu</i></a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="#main" data-bind="">Главная</a></li>
                         <li><a href="#categories" data-bind="">Категории</a></li>
                     </ul>
                 </div>
             </nav>
             {{-- side menu --}}
             <ul id="slide-out" class="sidenav">
+                <li><a class="waves-effect sidenav-close" href="#main" data-bind="">Главная</a></li>
                 <li><a class="waves-effect sidenav-close" href="#categories" data-bind="">Категории</a></li>
             </ul>
             {{-- progress bar --}}
@@ -135,12 +138,19 @@
 
             // events for pages visualisation
             let mainVisibleEvent = new Event("MainVisible");
+            let categoriesVisibleEvent = new Event("CategoriesVisible");
 
             RootViewModel.sections.main.subscribe(function (newValue) {
-            if (newValue) {
-                document.dispatchEvent(mainVisibleEvent);
-            }
-        });
+                if (newValue) {
+                    document.dispatchEvent(mainVisibleEvent);
+                }
+            });
+
+            RootViewModel.sections.categories.subscribe(function (newValue) {
+                if (newValue) {
+                    document.dispatchEvent(categoriesVisibleEvent);
+                }
+            });
         </script>
     </body>
 </html>
