37f5a13b7574c2477663bc55ba9c41352815d062
Efremenko Alex <alexeyefr@gmail.com>
2019-08-07 00:40:14 +0300

    Refactor: change method from POST to GET for api/user/get

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::group(['namespace' => 'Api'], function () {
         Route::post('login', 'LoginController')->name('login');
         Route::get('logout', 'LogoutController')->middleware('auth:api');
     });
-    Route::post('user/get', 'ManageUserController@getUser')->middleware('auth:api');
+    Route::get('user/get', 'ManageUserController@getUser')->middleware('auth:api');
     Route::post('user/save/contacts', 'ManageUserController@saveUserContacts')->middleware('auth:api');
     Route::post('user/password/change', 'ManageUserController@changeUserPassword')->middleware('auth:api');
     Route::get('user/checkout', 'ManageUserController@checkout')->middleware('auth:api');
