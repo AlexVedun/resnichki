78df2ea30a4a8b4802230ad62e65bc08d2a30da4
Efremenko Alex <alexeyefr@gmail.com>
2019-08-09 00:50:08 +0300

    Refactor: continue to create system to manage offers

--- app/Http/Controllers/Api/ManageOffersController.php
+++ app/Http/Controllers/Api/ManageOffersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Helpers\GlobalFunctions;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Offer;
 
 class ManageOffersController extends Controller
 {
@@ -16,14 +18,24 @@ class ManageOffersController extends Controller
         $offers = $user->offers;
         foreach ($offers as $offer)
         {
-            $offer->cover = GlobalFunctions::ConvertImage2base64('//media//covers//'.$offer->cover);
+            //$offer->cover = GlobalFunctions::ConvertImage2base64('//covers//'.$offer->cover);
+            $offer->cover = env('APP_URL', false).Storage::url($offer->cover);
         }
         return $offers;
     }
 
-    public function saveOffer(Request $request)
+    public function saveOffers(Request $request)
     {
-        $path = $request->file('cover_file')->store('media/covers');
-        return $path;
+        $offer = Offer::find($request->offer_id);
+        $offer->title = $request->title;
+        $offer->short_descr = $request->short_descr;
+        $offer->category_id = $request->category_id;
+        if (json_decode($request->is_cover_change))
+        {
+            $offer->cover = $request->file('cover_file')->store('public/media/covers');
+        }
+        $offer->save();
+        //$path = $request->file('cover_file')->store('public/media/covers');
+        //return env('APP_URL', false).Storage::url($path);
     }
 }
