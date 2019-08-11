b496d095ccf0ccc524d839807db12238a21026e2
Efremenko Alex <alexeyefr@gmail.com>
2019-08-11 16:59:20 +0300

    Fix: fix relations between entities

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -27,7 +27,7 @@ class Offer extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class, 'category_id', 'id');
+        return $this->belongsTo(Category::class, 'id', 'category_id');
     }
 
     public function user()
